4e5a2b87c2645f6c132f8ca004dd4b51f6945e36
visorde-9e-ay2526 <visorde.cm@obmontessori.edu.ph>
2026-04-24 10:06:41 +0800

    update main

---
+++ main.js
@@ -0,0 +1,6 @@
+function hayopCounter (){
+    var hayop = ["lion", "flamingo", "tiger", "boa constrictor"];
+        for (var i=0; i < hayop.length; i++){
+            window.alert("This zoo contains a " + hayop[i] + ".");
+        }
+}
